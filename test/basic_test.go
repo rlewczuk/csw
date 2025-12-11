abe5e13ac7fa935c0c279be50325e8e8955651fa
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2025-12-11 08:50:09 +0100

    More docs, planning, skeletal project.

---
+++ test/basic_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestAgentCoreInitialization(t *testing.T) {
+	t.Run("basic initialization", func(t *testing.T) {
+		// TODO: implement agent core initialization test
+		// when agent core implementation is ready
+		t.Skip("Agent core not implemented yet")
+	})
+}
