03748caf3601a08cc3dce9b7fa855062313ca896
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2026-01-04 01:52:23 +0100

    Rudimentary agent code (manual parts)

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -1,26 +1,50 @@
 package core
 
 import (
-	"io"
-
+	"github.com/codesnort/codesnort-swe/pkg/models"
+	"github.com/codesnort/codesnort-swe/pkg/tool"
 	"github.com/codesnort/codesnort-swe/pkg/vfs"
 )
 
-type SweSystem interface {
-	io.Closer
-	NewProject(root string) SweProject
+// SweSystem represents the core system for managing conversations, tools, and models.
+type SweSystem struct {
+
+	// Map of model providers by name
+	ModelProviders map[string]models.ModelProvider
+
+	// System prompt template
+	SystemPrompt string
+
+	// Tool registry
+	Tools *tool.ToolRegistry
+
+	// Virtual filesystem
+	VFS vfs.VFS
 }
 
-type SweProject interface {
-	NewSession() SweSession
+func (s *SweSystem) NewSession(model string) (*SweSession, error) {
+	// TODO implement me
+	return nil, nil
 }
 
-type SweTask interface {
-	Subtasks() []SweTask
+type SweSession struct {
+	// TODO add missing fields
 }
 
-type SweSession interface {
-	SetRole(role string) error
-	Prompt(prompt string) error
-	Vfs() vfs.VFS
+// Prompt adds user prompt to the conversation and starts processing if processing is not already in progress.
+// If processing is already in progress, if will be added at the end of conversation after current LLM request is completed,
+// its tool calls are executed etc. Returns immediately.
+func (s *SweSession) UserPrompt(prompt string) error {
+	// TODO implement me
+	return nil
+}
+
+func (s *SweSession) Run() error {
+	// TODO implement me
+	return nil
+}
+
+func (s *SweSession) ChatMessages() []*models.ChatMessage {
+	// TODO implement me
+	return nil
 }
